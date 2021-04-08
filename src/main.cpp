b7f16e56c0573878b7825cdf609d9d519ce5cde9
Andreas Bielawski <brawl@nyanya.de>
2021-04-08 23:55:32 +0200

    Port to Aroma

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,67 @@
 #include <wups.h>
-#include <malloc.h>
-#include <string.h>
-#include <nsysnet/socket.h>
 #include <utils/logger.h>
+#include <cstring>
+#include <nsysnet/socket.h>
 #include <coreinit/title.h>
 #include <curl/curl.h>
+#include <whb/log_udp.h>
 
-#define HOMESERVER "tag.rc24.xyz"
+#define SERVER "tag.rc24.xyz"
 
 WUPS_PLUGIN_NAME("UTag");
-WUPS_PLUGIN_DESCRIPTION("Display the titles you play on your tag!");
-WUPS_PLUGIN_VERSION("v1.0.1");
-WUPS_PLUGIN_AUTHOR("twosecslater");
+WUPS_PLUGIN_DESCRIPTION("Display the last played titles on your RiiTag!");
+WUPS_PLUGIN_VERSION("v2.0");
+WUPS_PLUGIN_AUTHOR("twosecslater, Brawl345");
 WUPS_PLUGIN_LICENSE("GPLv3");
 
-WUPS_FS_ACCESS();
+WUPS_USE_WUT_DEVOPTAB();
 
 char key[129];
 
 INITIALIZE_PLUGIN() {
-    int fd=-1;
-    fd = open("sd:/utag.txt", O_RDONLY);
-    read(fd, key, 128);
-    close(fd);
+    WHBLogUdpInit();
+
+    FILE *fp = fopen("fs:/vol/external01/wiiu/utag.txt", "r");
+    if (!fp) {
+        DEBUG_FUNCTION_LINE("utag.txt not found in SD://wiiu/")
+        return;
+    }
+    fread(key, 128, 1, fp);
+    fclose(fp);
 }
 
-ON_APPLICATION_START(args) {
+ON_APPLICATION_START() {
+    WHBLogUdpInit();
+
+    if (strlen(key) == 0) {
+        DEBUG_FUNCTION_LINE("Key not loaded, so ignoring.");
+        return;
+    }
+
     socket_lib_init();
-    log_init();
     char tempTID[17];
     char TID[17];
     char type[9];
     sprintf(tempTID, "%llx", OSGetTitleID());
     sprintf(TID, "%016s", tempTID);
     snprintf(type, sizeof(type), TID);
+    DEBUG_FUNCTION_LINE("UTAG: tempTID is %s", tempTID);
+    DEBUG_FUNCTION_LINE("UTAG: TID is %s", TID);
+    DEBUG_FUNCTION_LINE("UTAG: Type is %s", type);
+
     char tagURL[180];
-    snprintf(tagURL, sizeof(tagURL), "http://%s/wiiu?game=%s&key=%s", HOMESERVER, TID, key);
+    snprintf(tagURL, sizeof(tagURL), "http://%s/wiiu?game=%s&key=%s", SERVER, TID, key);
+    DEBUG_FUNCTION_LINE("Tag URL is %s", tagURL);
     if (strcmp(type, "00050000") == 0) {
+        DEBUG_FUNCTION_LINE("UTAG: Get ready for cURL!");
         CURL *curl = curl_easy_init();
         CURLcode ec;
         curl_easy_setopt(curl, CURLOPT_URL, tagURL);
         curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5L);
         ec = curl_easy_perform(curl);
-        if(ec != CURLE_OK) {
+        if (ec != CURLE_OK) {
             DEBUG_FUNCTION_LINE("curl failed with exit code %s", curl_easy_strerror(ec));
-        }   
+        }
         curl_easy_cleanup(curl);
-        //curl_global_cleanup();
     }
-} 
-
+}
