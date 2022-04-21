0235a86002a59a512a749818f070c85eaa3870c2
DannyAAM <danny@saru.moe>
2022-04-21 17:10:27 +0800

    Fix for vc injection with reworking some code
    * fix for missing submission for vc injections
    * add support for submitting real game id for UWUVCI
    * rework the code for checking title id by bitwise and relational
    operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <wups.h>
 #include <malloc.h>
-#include <string.h>
 #include <nsysnet/socket.h>
 #include <utils/logger.h>
 #include <coreinit/title.h>
 #include <curl/curl.h>
+
+#include <nn/acp/title.h>
 
 #define HOMESERVER "tag.rc24.xyz"
 
@@ -28,15 +29,31 @@ INITIALIZE_PLUGIN() {
 ON_APPLICATION_START(args) {
     socket_lib_init();
     log_init();
-    char tempTID[17];
+    uint64_t title_id = OSGetTitleID();
+    uint64_t t_type = title_id >> 32;
+    uint64_t t_gid = title_id & 0xffffffff;
+    uint64_t t_gid_high = t_gid >> 16;
+    uint64_t t_gid_low = t_gid & 0xffff;
+    bool is_uwuvci = false;
     char TID[17];
-    char type[9];
-    sprintf(tempTID, "%llx", OSGetTitleID());
-    sprintf(TID, "%016s", tempTID);
-    snprintf(type, sizeof(type), TID);
+    if (t_type == 0x00050002 &&
+            (t_gid_high > 0x3000 && t_gid_high < 0x10000) &&
+            (t_gid_low > 0x3000 && t_gid_low < 0x10000)) { // UWUVCI AIO Injected game
+        ACPMetaXml meta;
+        ACPGetTitleMetaXml(title_id, &meta);
+        // transform it to (TeconMoon's) vc injection id format
+        sprintf(TID, "%08lx%08lx", t_type, meta.reserved_flag2);
+        is_uwuvci = true;
+    } else { // normal wiiu tid
+        sprintf(TID, "%016llx", title_id);
+    }
     char tagURL[180];
     snprintf(tagURL, sizeof(tagURL), "http://%s/wiiu?game=%s&key=%s", HOMESERVER, TID, key);
-    if (strcmp(type, "00050000") == 0) {
+    if (t_type == 0x00050000 || // normal wii u titles
+            is_uwuvci ||
+            // (TeconMoon's) vc injections
+            // (should excluded all demos accroding to https://wiiubrew.org/wiki/Title_database#00050002:_Kiosk_Interactive_Demo_and_eShop_Demo @ 21/4/2022)
+            (t_type == 0x00050002 && (t_gid_high < 0x1010 || t_gid_high > 0x1021))) {
         CURL *curl = curl_easy_init();
         CURLcode ec;
         curl_easy_setopt(curl, CURLOPT_URL, tagURL);
