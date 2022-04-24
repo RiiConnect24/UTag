b6dcd3376fb38b5215695dcce43291e7756874d8
DannyAAM <danny@saru.moe>
2022-04-24 21:54:58 +0800

    real fix for the alignment issue
    also thanks for the advancement of c++11

--- src/main.cpp
+++ src/main.cpp
@@ -35,41 +35,15 @@ ON_APPLICATION_START(args) {
     uint16_t t_gid_high = t_gid >> 16;
     uint16_t t_gid_low = t_gid & 0xffff;
     bool is_uwuvci = false;
-    /*
-     * should be reserved_flag2 according to meta.xml
-     * but seems there's some bug related to memory alignment issue?
-     * (for ACPGetTitleMetaXml, which the address of ACPMetaXml is somehow critical)
-     * the TID is actually only required to be 17bytes,
-     * but added with 16 more bytes to make the alignment of ACPMetaXml is able to work.
-     * note: it's werid that if you try to make it just 8 bytes more,
-     * which should make the ACPMetaXml aligned perfectly,
-     * it just shifted wayyyyy more, which is totally not working.
-     */
-    char TID[33];
-    TID[17] = 0; // make sure it always 16 chars, may not required?
+    char TID[17];
     if (t_type == 0x00050002 &&
             (t_gid_high > 0x3000 && t_gid_high < 0x10000) &&
             (t_gid_low > 0x3000 && t_gid_low < 0x10000)) { // UWUVCI AIO Injected game
-        ACPMetaXml meta;
+        // need to be aligned to 0x40 for IOSU
+        alignas(0x40) ACPMetaXml meta;
         ACPResult result = ACPGetTitleMetaXml(title_id, &meta);
         if (result == ACPResult::ACP_RESULT_SUCCESS) {
-
-            /*
-             * this is the code i used to find offset
-             * removing this code seems to also affect the offset...
-             * so i decide to leave it there
-             */
-            char* buf = (char*)&(meta.reserved_flag2);
-            char* buf2 = TID;//key;
-            for (int i = 0; i < 8/*64*/; i++)
-            {
-                buf2 += sprintf(buf2, "%02X", buf[i]);
-            }
-            *buf2 = 0;
-
-            // there's 8 bytes offset, thus make reserved_flag2 becomes reserved_flag4
-            //uint32_t real_tid = meta.reserved_flag2;
-            uint32_t real_tid = meta.reserved_flag4;
+            uint32_t real_tid = meta.reserved_flag2;
             char real_t_type = (char)(real_tid >> 24);
             if ((real_t_type > 'A' && real_t_type < 'Z') || // should be uppercase letter for correct titles
                     (real_t_type > '0' && real_t_type < '9')) { // only 091E00 falls into this?
